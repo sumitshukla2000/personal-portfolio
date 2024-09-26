99e621f7fb53485dca69763a48b1dcd7f45d8f2f
sumitshukla2000 <shuklasumit93@gmail.com>
2024-09-27 02:13:24 +0530

    Avatar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { useState , useRef } from 'react'
 import { navLinks } from './constants'
+import avatar from '../assets/man.png'
 import 'primeicons/primeicons.css';
 
 function Header() {
-
+    const [active , setActive] = useState('Home')
     const [toggleMenu , setToggleMenu] = useState(false)
     
   return (
@@ -12,22 +13,22 @@ function Header() {
         <nav className='md:w-full w-[100%] flex items-center justify-between mx-auto px-4'>
             
             <div className='mx-4'>
-                logo
+                <img src={avatar} alt="logo" className='h-[40px] w-[40px]'/>
             </div>
 
             <div className='md:w-[50%] md:block hidden w-[80%]'>
-                <ul className='flex items-center md:justify-evenly  font-body font-bold md:text-[1.2rem] text-[0.8rem]'>
+                <ul className='flex items-center md:justify-evenly font-body font-bold md:text-[1.2rem] text-[0.8rem]'>
                     {navLinks.map(navLink => (
                         <li className='basis-1/5 mx-[10px]' key={navLink.id}>
-                            <a href="" className='hover:text-slate-500 transition ease-in-out delay-75 py-4'>{navLink.title.toUpperCase()}</a>
+                            <a href="#" className={`hover:text-blue transition ease-in-out delay-75 py-4 px-2 ${active === navLink.title ? "text-blue" : "text-black"}`} onClick={()=>setActive(navLink.title)}>{navLink.title.toUpperCase()}</a>
                         </li>
                     ))}
                 </ul>
             </div>
 
-            <div className='md:hidden hover:rounded-full bg-slate-300 px-2 py-1'>
+            <div className='md:hidden hover:rounded-2xl hover:bg-slate-300 px-2 py-1'>
             <button onClick={() => setToggleMenu(prev => !prev)}>
-            <i className={`pi pi-${toggleMenu ? 'times' : 'align-justify'} text-2x  `} ></i>
+            <i className={`pi pi-${toggleMenu ? 'times' : 'align-justify'} text-2xl  `} ></i>
                 </button>
             </div>
         </nav>
@@ -39,12 +40,14 @@ function Header() {
         <ul className='font-bold text-[0.89rem] font-display flex items-center flex-col'>
                     {navLinks.map(navLink => (
                         <li className='basis-1/4h-[40px] flex justify-end px-8 py-1 text-center border-b border-slate-300 w-full' key={navLink.id}>
-                            <a href="" className='hover:text-slate-500 transition ease-in-out delay-75 py-4 tracking-[0.2rem]'>{navLink.title.toUpperCase()}</a>
+                            
+                            <a href="#" className="hover:text-slate-500 transition ease-in-out delay-75 py-4 tracking-[0.2rem]">
+                                {navLink.title.toUpperCase()}
+                                 </a>
                         </li>
                     ))}
-                </ul>
+                     </ul>
         </div>
-
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import avatar  from '../assets/avatar.jpg'
-import { Button } from 'primereact/button';  
+import avatar  from '../assets/man.png'
+// import { Button } from 'primereact/button';  
 
 
 function Hero() {
@@ -8,25 +8,31 @@ function Hero() {
         <>
             <div className='w-full h-[70vh]'>
 
-                {/* <div className='w-full pt-24'>
-                    <div className='h-[210px] w-[210px] bg-blue rounded-full mx-auto flex items-center'>
-                    <img src={avatar} alt="avatar" className='w-[200px] h-[200px] mx-auto rounded-full'/>
+                <div className='w-full pt-16'>
+                    <div className='h-[200px] w-[200px] bg-blue rounded-full mx-auto flex items-center'>
+                    <img src={avatar} alt="avatar" className='w-[190px] h-[190px] mx-auto rounded-full'/>
                     </div>
-                </div> */}
+                </div>
                 
 
-                <div className='w-full font-display pt-32'>
+                <div className='w-full font-display pt-4'>
                     <p className='tracking-[0.3rem] text-6xl font-bold text-center'>HAY, I'M <span className='text-blue z-5'  
                     style={{ textShadow: "5px 6px #6E07F3" }}>SUMIT</span> SHUKLA</p>
                 </div>
 
-                <div className='w-[80%] font-body py-8 mx-auto'>
+                <div className='w-[60%] font-body pt-4 mx-auto'>
             <p className='text-xl font-medium text-center p-4'>"Crafting Seamless Web Experience: Responsive 
                  <span className='text-blue font-extrabold' style={{ textShadow: "1px 1px #6E07F3" }}> Frontend</span> Development with Precision and Creativity"</p>
                 </div>
 
-                <div className="card flex justify-content-center">
+                {/* <div className="card flex justify-content-center">
                 <Button  label="Submit" />
+        </div> */}
+
+        <div className='w-[80%] mx-auto my-4 flex items-center justify-center'> 
+            <a href='#' className='font-body font-extralight text-white bg-blue py-2 px-12 text-md tracking-[2px] rounded-md  shadow-slate-500 transition ease-in-out focus:outline-none delay-50 hover:shadow-lg hover:translate-y-[-3px]'>
+                PROJECTS
+            </a>
         </div>
         
             </div>
